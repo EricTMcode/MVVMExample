f86997f490f07862a019f458ca82f65cf50cfc88
Eric <ebagnaro@gmail.com>
2024-02-17 07:36:13 +0100

    Initial Commit

---
+++ MVVMExample/MVVMExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVVMExampleApp.swift
+//  MVVMExample
+//
+//  Created by Eric on 17/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct MVVMExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
